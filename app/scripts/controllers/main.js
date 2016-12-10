4404a55e3628d58bd4a1fae71da906e022a54122
CamilleT <camille.trinquart.02@etu.unilim.fr>
2016-12-10 18:30:00 +0100

    Tuto part 3.1

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,9 +12,78 @@ angular.module('movieExplorerApp')
     var ctrl = this;
 
     ctrl.movies = [];
+	ctrl.slides = [];
 
     movies.getDiscoverMovies().then(function(result)
     {
       ctrl.movies = result;
+	  
+	  movies.getConfiguration().then(function(result)
+	  {
+		  ctrl.configuration = result;
+	  }).then(function(){
+		  angular.forEach(ctrl.movies, function(movie)
+		  {
+			  if(movie.backdrop_path)
+			  {
+				  ctrl.slides.push(ctrl.getImageURL(movie.backdrop_path,'w780'));
+			  }
+		  });
+		  ctrl.currentSlide = 0;
+	  })
     });
+	
+	ctrl.getImageURL = function(path, size)
+	{
+		
+		if(ctrl.configuration)
+			{
+				return ctrl.configuration.images.secure_base_url + size + path;
+			}else{
+				return null;
+			}
+	};
+	
+	ctrl.getNextSlideIndex = function()
+	{
+		if(ctrl.currentSlide == ctrl.slides.length -1)
+		{
+			return 0;
+		}
+		return ctrl.currentSlide + 1;
+	};
+	
+	ctrl.getPreviousSlideIndex = function()
+	{
+		if(ctrl.currentSlide == 0)
+		{
+			return ctrl.slides.length -1;
+		}
+		return ctrl.currentSlide - 1;
+	}
+	
+	ctrl.slideIsVisible = function(index)
+	{
+		return index == ctrl.currentSlide || index == ctrl.getPreviousSlideIndex()
+		|| index == ctrl.getNextSlideIndex();
+	}
+	
+	ctrl.getSlideClass = function(index)
+	{
+		if(index == ctrl.currentSlide)
+		{
+			return 'current';
+		}else if(index == ctrl.getPreviousSlideIndex()){
+			return 'previous';
+		}else if(index == ctrl.getNextSlideIndex())){
+			return 'next';
+		}
+		return '';
+	}
+	
+	ctrl.gotoSlide = function(index)
+	{
+		ctrl.currentSlide = index;
+	}
+	
   });
